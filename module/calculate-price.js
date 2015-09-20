c290779ca72376bb3b732876e4c30bbad6c36d36
laetella <laetella@outlook.com>
2015-09-21 01:38:42 +0800

    according to computing method 1 to add method getSubTotalPrice and getTotalPrice to calculate total price

--- module/calculate-price.js
+++ module/calculate-price.js
@@ -1,18 +1,36 @@
-function CalculatePrice() {
-
+function CalculatePrice(allBooks) {
+  this.allBooks = allBooks;
+  this.keyArray = Object.keys(this.allBooks);
+  this.kinds = this.getKindsNum();
+  this.groups = this.getGroupsNum();
+  this.allPrice = [];
 }
 
-CalculatePrice.prototype.getKindsNum = function(allBooks) {
+CalculatePrice.prototype.getKindsNum = function() {
   var kinds = 0;
-  for(var keys in allBooks) {
-    kinds ++;
+  for (var item = 0; item < this.keyArray.length; item++) {
+    if (this.allBooks[this.keyArray[item]] !== 0) {
+      kinds++;
+    }
   }
   return kinds;
-}
+};
 
-CalculatePrice.prototype.getSinglePrice = function(kind) {
+CalculatePrice.prototype.getGroupsNum = function() {
+  var groups = this.allBooks[this.keyArray[0]];
+  for (var group = 1; group < this.kinds; group++) {
+    if (this.allBooks[this.keyArray[group]] !== 0) {
+      if (this.allBooks[this.keyArray[group]] > groups) {
+        groups = this.allBooks[this.keyArray[group]];
+      }
+    }
+  }
+  return groups;
+};
+
+CalculatePrice.prototype.getSinglePrice = function(kinds) {
   var discount = 0;
-  switch (kind) {
+  switch (kinds) {
     case 1:
       discount = 0;
       break;
@@ -29,7 +47,23 @@ CalculatePrice.prototype.getSinglePrice = function(kind) {
       discount = 2.0;
       break;
   }
-  return kind * (8.00 - discount);
+  return kinds * (8.00 - discount);
+};
+
+CalculatePrice.prototype.getSubTotalPrice = function(kinds) {
+  var subTotal = this.getSinglePrice(kinds);
+  for (var kind = this.kinds; kind > 0; kind--) {
+    this.allBooks[this.keyArray[kind]]--;
+  }
+  return subTotal;
+};
+
+CalculatePrice.prototype.getTotalPrice = function(allBooks) {
+  var sum = 0;
+  for (var each = 0; each < this.groups; each++) {
+    sum += this.getSubTotalPrice(this.getKindsNum());
+  }
+  return sum;
 };
 
 module.exports = CalculatePrice;
