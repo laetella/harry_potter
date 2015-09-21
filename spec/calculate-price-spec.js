c0c4e133d5d68c107b73b131739baa13191f8540
laetella <laetella@outlook.com>
2015-09-21 13:31:56 +0800

    add test getTotalPrice and getLowestPrice method

--- spec/calculate-price-spec.js
+++ spec/calculate-price-spec.js
@@ -26,8 +26,13 @@ describe('harry_potter test', function() {
   // });
 
   it('should get correct total price', function() {
-    var totalPrice = calculatePrice.getTotalPrice(allBooks);
+    var totalPrice = calculatePrice.getTotalPrice(allBooks,0);
     expect(totalPrice).toBe(51.60);
+  });
+
+  it('should get correct lowest price', function() {
+    var totalPrice = calculatePrice.getLowestPrice(allBooks);
+    expect(totalPrice).toBe(51.20);
   });
 
 });
